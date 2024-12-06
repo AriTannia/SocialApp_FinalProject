628328a6c0de25bb76c3863a9dbc757d19f21907
DLGiaKiet <giakiet20012004@gmail.com>
2024-12-06 17:03:46 +0700

    updating sqlite for users chat list

--- SocialApp_DACK/app/src/main/java/tannguyen/st/ueh/edu/vn/socialapp_dack/adapters/ImageAdapter.java
+++ SocialApp_DACK/app/src/main/java/tannguyen/st/ueh/edu/vn/socialapp_dack/adapters/ImageAdapter.java
@@ -2,37 +2,33 @@ package tannguyen.st.ueh.edu.vn.socialapp_dack.adapters;
 
 import android.content.Context;
 import android.text.TextUtils;
-import android.widget.Toast;
 
 import tannguyen.st.ueh.edu.vn.socialapp_dack.utils.ImageDownloader;
 
 public class ImageAdapter {
 
     public interface SaveImageCallback {
-        void onImageSaved(String filePath); // Trả về đường dẫn khi ảnh được lưu
-        void onError(Exception e); // Xử lý lỗi
+        void onImageSaved(String filePath); // Giao diện callback trả về file path
+        void onError(Exception e); // Giao diện callback trả về lỗi
     }
 
-    public static void saveImageToInternalStorage(Context context, String profileOrCoverPhoto, String imageUrl, SaveImageCallback callback) {
-
-        // Kiểm tra URL trước khi xử lý
+    public static void saveImageToInternalStorage(Context context, String uid, String profileOrCoverPhoto, String imageUrl, SaveImageCallback callback) {
         if (TextUtils.isEmpty(imageUrl) || "null".equals(imageUrl)) {
             callback.onError(new IllegalArgumentException("URL ảnh không hợp lệ."));
             return;
         }
 
-        String fileName = profileOrCoverPhoto.equals("image") ? "profile_image.jpg" : "cover_image.jpg";
+        // Tạo tên file dựa trên uid
+        String fileName = profileOrCoverPhoto.equals("image") ? uid + "_profile_image.jpg" : uid + "_cover_image.jpg";
 
         ImageDownloader.downloadImage(context, imageUrl, fileName, new ImageDownloader.DownloadCallback() {
             @Override
             public void onSuccess(String filePath) {
-                // Trả về đường dẫn qua callback
                 callback.onImageSaved(filePath);
             }
 
             @Override
             public void onError(Exception e) {
-                // Trả về lỗi qua callback
                 callback.onError(e);
             }
         });
